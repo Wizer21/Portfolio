4c01320c719fbd99b14387319e508f062fdc104b
Wizer <simon.tessier8@gmail.com>
2021-02-25 23:30:59 +0100

    Basic interface, data and first animation

--- js/index.js
+++ js/index.js
@@ -5,69 +5,146 @@ import { MTLLoader } from "../files/threejs/MTLLoader.js";
 
 var sphere_list = []
 
-const data = [
+const data = [    
     {
-        title: "Projet 1",
-        description: "JavaScript Object Notation est un format de données textuelles dérivé de la notation des objets du langage JavaScript. Il permet de représenter de l’information structurée comme le perm",
-        git_link: "https://github.com/Wizer21?tab=repositories",
+        title: "Swich Network",
+        description: "Learning project imitating a social network. This project was realized using Python and Django",
+        git_link: "https://github.com/Wizer21/Swich_network",
         start_date: "01/01/2020",
         end_date: "02/02/2021",
-        image: "../files/images/winter.jpg",
+        image: "../files/images/network.png",
         youtube: "null",
     },
     {
-        title: "Projet 2",
-        description: "amework that just works. Bulma is a free, open source framework that provides ready-to-use frontend components that you can easily combine to build respon",
-        git_link: "https://bulma.io/",
+        title: "Robot arm",
+        description: "This project is a 6-axis arm, controlled on a Raspberry Pi. The arm can be managed from an Xbox controller. 🎮 By using the calculation method below, I can create vertical or horizontal movements. If, for example, I want to claw to be lowered, I set this new position and calculate from this one, witch should be the new motors postions.",
+        git_link: "https://github.com/Wizer21/6Servo_Robot_Arm",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/arm.jpg",
+        youtube: "null",
+    },
+    {
+        title: "Tracking Robot",
+        description: "This project is realized on Raspberry Pi 4. This robot is equipped with 2 DC motor that allow it to move around. Two servomotor control the camera which can in this way follow objects. Both DC and servomotors can be controlled from the Qt interface or from an Xbox controller.",
+        git_link: "https://github.com/Wizer21/TrackerRobot",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/robot_tracking.jpg",
+        youtube: "null",
+    },
+    {
+        title: "Synchronized Canvas",
+        description: "This project is a simple scribble application. Users can create rooms that will synchronize their canvas with the users present in the same room.",
+        git_link: "https://github.com/Wizer21/Connected_canvas",
         start_date: "01/01/2045",
         end_date: "02/02/2019",
-        image: "../files/images/witch.png",
+        image: "../files/images/canvas.png",
+        youtube: "null",
+    },
+    {
+        title: "Command Generator",
+        description: "'Command Generator' is an application that generates order mail in a few clicks.",
+        git_link: "https://github.com/Wizer21/OrderGenerator",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/table_pres.jpg",
+        youtube: "null",
+    },
+    {
+        title: "Image/Video Tracker",
+        description: "This program allows you to find complex shapes from an image, depending on their color, by clicking on the image, the algorithm will get the pixel table of our image, the position of pixel clicked and its color",
+        git_link: "https://github.com/Wizer21/Image_Tracker",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/green.png",
+        youtube: "null",
+    },
+    {
+        title: "TicTacToe",
+        description: "This TicTacToe allows you to play against the program that analyzes the board to find the best way to deafeat you.",
+        git_link: "https://github.com/Wizer21/TicTacToe",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/tictactoe.jpg",
+        youtube: "null",
+    },
+    {
+        title: "Swich",
+        description: "This project was mainly made to showcase my skills and keep an history of my progression. The developpement started on 2nd, november 2020. This project was only made through C++ with the Qt library.",
+        git_link: "https://github.com/Wizer21/Swich",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/swich.jpg",
+        youtube: "null",
+    },
+    {
+        title: "Simulation Revendeur",
+        description: "First Qt Application",
+        git_link: "https://github.com/Wizer21/SimulationRevendeur",
+        start_date: "01/01/2020",
+        end_date: "02/02/2021",
+        image: "../files/images/Revendeur.jpg",
         youtube: "null",
     },
 ];
 
 export function main() {
     document.addEventListener("DOMContentLoaded", () => {
+        // BUILD CURSOR 
+        const cursor = document.getElementById("cursor_custom")
+        document.addEventListener("mousemove", event => {
+            cursor.setAttribute("style", `top: ${event.pageY}px; left: ${event.pageX}px;`)
+        })
+
+        // BUILD INTERFACE 
         let body = document.getElementById("body_container");
-        let container_3D = document.getElementById("3d_container");
+        let container_3D = document.getElementById("container_3d");
 
         let new_div;
         let new_elem_text;
         let new_elem_image;
         let new_elem;
         for (let i = 0; i < data.length; i++) {
-        // BUILD DIVS
-        new_div = document.createElement("div");
-        new_elem_text = document.createElement("div");
-        new_elem_image = document.createElement("div");
+            // BUILD DIVS
+            new_div = document.createElement("div");
+            new_elem_text = document.createElement("div");
+            new_elem_image = document.createElement("div");
 
-        // BUILD IMAGE
-        new_elem = document.createElement("img");
-        new_elem.src = data[i].image;
-        new_elem_image.appendChild(new_elem);
+            // BUILD IMAGE LINK
+            new_elem = document.createElement("a");
+            new_elem.href = data[i].git_link;
+            new_elem.target = "_blank"
+            new_elem_image.appendChild(new_elem);
+            // BUILD IMAGE
+            new_elem = document.createElement("img");
+            new_elem.src = data[i].image;
+            new_elem.className = "row"
+            new_elem_image.children[0].appendChild(new_elem);
 
-        // BUILD DESCRIPTION
-        new_elem_text.appendChild(build_element_with_text(new_elem, "h1", data[i].title));
-        new_elem_text.appendChild(build_element_with_text(new_elem, "p", data[i].description));
-        new_elem_text.appendChild(build_element_with_text(new_elem, "p", "Start " + data[i].start_date));
-        new_elem_text.appendChild(build_element_with_text(new_elem, "p", "End " + data[i].end_date));
+            // BUILD DESCRIPTION
+            new_elem_text.appendChild(build_element_with_text(new_elem, "h1", data[i].title));
+            new_elem_text.appendChild(build_element_with_text(new_elem, "p", data[i].description));
+            new_elem_text.appendChild(build_element_with_text(new_elem, "p", "Start " + data[i].start_date));
+            new_elem_text.appendChild(build_element_with_text(new_elem, "p", "End " + data[i].end_date));
 
-        // SET POSITION
-        new_div.className = "parent";
-        if (i % 2 == 0) {
-            new_elem_text.className = "right_col";
-            new_elem_image.className = "left_col";
-        } else {
-            new_elem_text.className = "left_col";
-            new_elem_image.className = "right_col";
-        }
+            // SET POSITION
+            new_div.className = "parent row";
+            if (i % 2 == 0) {
+                new_elem_text.className = "right_col text_div";
+                new_elem_image.className = "left_col";
+            } else {
+                new_elem_text.className = "left_col text_div";
+                new_elem_image.className = "right_col";
+            }
 
-        // STRUCTURE
-        new_div.appendChild(new_elem_text);
-        new_div.appendChild(new_elem_image);
-        body.appendChild(new_div);
+            // STRUCTURE
+            new_div.appendChild(new_elem_text);
+            new_div.appendChild(new_elem_image);
+            body.appendChild(new_div);
         }
         render_scene(container_3D);
+        container_3D.size = container_3D.children.size
     });
 }
 
@@ -81,13 +158,10 @@ function render_scene(container_3D) {
     // Scene
     const scene = new THREE.Scene();
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
     // Camera
     const camera = new THREE.PerspectiveCamera(
         75,
-        window.innerWidth / window.innerHeight,
+        window.innerWidth / (window.innerHeight/2),
         0.1,
         1000
     );
@@ -95,17 +169,12 @@ function render_scene(container_3D) {
 
     // Render
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight/2);
     container_3D.appendChild(renderer.domElement);
 
     // Camera Controler
+    
     const controls = new OrbitControls(camera, container_3D);
-
-    // // Light
-    // const light = new THREE.PointLight( 0xc4c4c4 )
-    // light.intensity = 2
-    // light.position.set(50, 10, 40);
-    // scene.add( light )
 
     let simon
     const loader = new OBJLoader();
@@ -117,7 +186,7 @@ function render_scene(container_3D) {
     })    
 
     // Build default items
-    for (let i = 0; i < 5; i++ ){
+    for (let i = 0; i < 40; i++ ){
         create_sphere(scene)
     }
 
@@ -125,16 +194,16 @@ function render_scene(container_3D) {
     const animate = function () {
         requestAnimationFrame(animate);
 
-        simon.rotation.y += 0.001;
+        simon.rotation.y += 0.002;
         renderer.render(scene, camera);
 
-        var number = dice(0, 100)
-        if (number > 98){
-            create_sphere(scene)
-        }
-        else if (number < 2){
-            delete_sphere(scene, renderer)
-        }
+        // var number = dice(0, 100)
+        // if (number > 98){
+        //     create_sphere(scene)
+        // }
+        // else if (number < 2 && sphere_list.length > 3){
+        //     delete_sphere(scene, renderer)
+        // }
     }
     animate()
 }
@@ -144,12 +213,12 @@ function create_sphere(scene){
 
     // Create Sphere
     var geometry = new THREE.SphereGeometry(2, 2, 2)
-    var material = new THREE.MeshBasicMaterial( {color: 0xffff00} )
+    var material = new THREE.MeshBasicMaterial( {color: 0xffffff} )
     var sphere = new THREE.Mesh(geometry, material)
     
     // Create Light
     var light = new THREE.PointLight( 0xc4c4c4 )
-    light.intensity = 1
+    light.intensity = 0.3
 
     // Set position
     light.position.set(position[0], position[1], position[2])
@@ -161,16 +230,14 @@ function create_sphere(scene){
     // Display
     scene.add(light)
     scene.add(sphere)
+
 }
 
 function delete_sphere(scene, renderer){
     if (sphere_list.length > 0){
 
-        console.log(sphere_list)
         var sphere = scene.getObjectById(sphere_list[0][0])
         var light = scene.getObjectById(sphere_list[0][1])
-        console.log(sphere)
-        console.log(light)
 
         sphere.geometry.dispose()
         sphere.material.dispose()
@@ -178,8 +245,6 @@ function delete_sphere(scene, renderer){
         scene.remove(light)
         
         sphere_list.shift()     
-        console.log("pop")   
-        console.log(sphere_list)
 
         renderer.renderLists.dispose();
     }
